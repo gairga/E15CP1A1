29b60d8d1749b90cac8d4aede56ff7414557122b
Gabriela Ilarreta <gabrielailarreta@gmail.com>
2018-09-24 19:54:54 -0300

    Se agregaron los modelos y controllers user y sessions

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def logged?
+    session[:user_id].present? ? true : false
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :histories
 
   root "histories#index"
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
